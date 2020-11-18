965f4177627a69947bad407b57ed018362f8f3f7
dannyhoover <dofjrtd@gmail.com>
2020-11-17 17:58:54 -0800

    still trying to get server to work

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-//const PORT = process.env.PORT || 3000;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
